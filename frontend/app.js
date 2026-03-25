93011916f38be179172be4418f8e18576ab61bec
ISabela <isabelaortizr11@gmail.com>
2026-03-25 00:12:42 -0400

    variable api en render en front

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = window.APP_CONFIG?.API_BASE_URL || 'http://localhost:3001/api';
+const API_BASE_URL = window.APP_CONFIG?.API_BASE_URL || 'https://crm-ela.onrender.com';
 
 let state = {
   productos: [],
